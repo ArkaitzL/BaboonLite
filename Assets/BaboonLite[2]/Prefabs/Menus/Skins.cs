fc6833aba45f382f3a0c7c9ca387bc4c6e8b2bf0
ArkaitzL <127892239+ArkaitzL@users.noreply.github.com>
2023-11-13 14:02:09 +0100

    skins menu

---
+++ Assets/BaboonLite[2]/Prefabs/Menus/Skins.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BaboOnLite {
+
+    [DefaultExecutionOrder(-1)]
+    [AddComponentMenu("BaboOnLite/_Menus/Skins")]
+    [DisallowMultipleComponent]
+    [HelpURL("https://docs.google.com/document/d/1zPv7QP-ZyisadG5zREiMmzV7UWsYTUPZIPT0f_YlhSE/edit?usp=sharing")]
+    public class Skins : MonoBehaviour
+    {
+
+    }
+}
